ff7395864708ebb689a421090f436ac4aff46465
Coolkie <peterlin1129@gmail.com>
2024-03-04 11:50:14 +0800

    cute-store

---
+++ cat-store/website/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pymysql
+import os
+
+PASSWORD = os.getenv('PASSWORD')
+USER = os.getenv('USER')
+HOST = os.getenv('HOST')
+DB = os.getenv('DB')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def buy():
+    email = request.form['email']
+    connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, database=DB, cursorclass=pymysql.cursors.DictCursor)
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO `store` (`username`) VALUES (%s)"
+        cursor.execute(sql, (email))
+        connection.commit()
