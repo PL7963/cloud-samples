9a40057b0b5bc30d4c8dd0c62f48b1cf4f949165
Coolkie <peterlin1129@gmail.com>
2024-03-04 15:18:42 +0800

    modify cat-store

--- cat-store/website/main.py
+++ cat-store/website/main.py
@@ -18,6 +18,8 @@ def buy():
     email = request.form['email']
     connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, database=DB, cursorclass=pymysql.cursors.DictCursor)
     with connection.cursor() as cursor:
-        sql = "INSERT INTO `store` (`username`) VALUES (%s)"
+        sql = "INSERT INTO `contact` (`email`) VALUES (%s)"
         cursor.execute(sql, (email))
         connection.commit()
+    connection.close()
+    return 'success uwu'
